0ad8360657de6406cfcd122e304f459ce25e218a
YUHEI <80562226+aspiratio@users.noreply.github.com>
2024-07-29 10:27:03 +0900

    本に関するクラスを作成

---
+++ book_management_system/models/book.py
@@ -0,0 +1,34 @@
+# 値オブジェクト
+class BookName:
+    def __init__(self, value: str) -> None:
+        if len(value) == 0:
+            raise ValueError("名前を入力してください")
+        if len(value) > 10:
+            raise ValueError("名前は10文字以内です")
+
+
+# メインで使用するクラス
+class Book:
+    def __init__(self, name: BookName, genre: str) -> None:
+        self._name = name
+        self._genre = genre
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def genre(self):
+        return self._genre
+
+
+class RegisteredBook(Book):
+    def __init__(self, name: BookName, genre: str, is_checked_out: bool) -> None:
+        super().__init__(self, name, genre)
+        self.is_checked_out = is_checked_out
+
+
+class UnregisteredBook(Book):
+    def __init__(self, name: BookName, genre: str) -> None:
+        # Memo: Bookクラスと変わらないが、未登録の本のみを指定するメソッドを作るためにサブクラスを定義する
+        super().__init__(self, name, genre)
