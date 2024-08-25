45992510b56bfc7893ba1961de9a2d0636de6195
YUHEI <80562226+aspiratio@users.noreply.github.com>
2024-08-25 21:36:44 +0900

    モックデータ作成処理まで追加

---
+++ book_management_system_flask/app/tests/conftest.py
@@ -0,0 +1,55 @@
+import os
+import tempfile
+
+import pytest
+from app import create_app
+from app.db import get_engine, init_db
+from sqlmodel import Session
+from app.models import User, Book, Loan
+
+
+def _add_mock_data():
+    engine = get_engine()
+    with Session(engine) as session:
+        mock_data = [
+            User(name="太郎", is_admin=0),
+            User(name="サトシ", is_admin=1),
+            Book(name="スッキリわかるJAVA入門", genre="技術書"),
+            Book(name="ハンターハンター", genre="マンガ"),
+            Loan(
+                book_id=1,
+                user_id=1,
+                loan_date="2024-08-01",
+                return_due_date="2024-09-01",
+            ),
+            Loan(
+                book_id=2,
+                user_id=2,
+                loan_date="2024-08-02",
+                return_due_date="2024-09-02",
+                return_date="2024-08-10",
+            ),
+        ]
+        session.add_all(mock_data)
+        session.commit()
+
+
+@pytest.fixture
+def test_app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({"TESTING": True, "DATABASE_URL": db_path})
+
+    with app.app_context():
+        init_db()
+        _add_mock_data()
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def test_client(app):
+    return app.test_client()
